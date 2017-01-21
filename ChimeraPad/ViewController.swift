32176a69d439697a5fceb1d6c9b73de198417005
Wayne Rittimann, Jr <johnwayner@gmail.com>
2017-01-20 20:23:57 -0600

    Initial Commit

---
+++ ChimeraPad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChimeraPad
+//
+//  Created by Wayne Rittimann, Jr. on 1/20/17.
+//  Copyright © 2017 Wayne Rittimann, Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
